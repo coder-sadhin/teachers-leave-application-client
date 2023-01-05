191ef64850bbe86cd4b1f9446df995ee6ae0f910
coder-sadhin <codersadhin@gmail.com>
2023-01-05 21:49:50 +0600

    add router

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layout/Main';
+import LoginForm from '../pages/LoginForm/LoginForm';
+import SingUpForm from '../pages/SingUpForm/SignUpForm';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Main />
+        element: <Main />,
+        children: [
+            {
+                path: '/',
+                element: <LoginForm />
+            },
+            {
+                path: '/login',
+                element: <LoginForm />
+            },
+            {
+                path: '/register',
+                element: <SingUpForm />
+            }
+        ]
+
     }
 ])
 
