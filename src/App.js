2af1d7f61174b5844babc3b676f8261334353b03
coder-sadhin <codersadhin@gmail.com>
2023-01-05 15:48:28 +0600

    ki hocche

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import router from './Router/router';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Developers</h1>
-      <p>Akkash</p>
-      <p>Sadikul </p>
-      <p>Nasim Billah</p>
-      <p>Najmus sakib</p>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+      <Toaster />
     </div>
   );
 }
