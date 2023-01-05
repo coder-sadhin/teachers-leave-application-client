4ddeb4f650f5df2d0513e168d97dcf4de7181ffb
coder-sadhin <codersadhin@gmail.com>
2023-01-05 22:26:25 +0600

    add context and query

---
+++ src/ContextApi/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,81 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {
+    createUserWithEmailAndPassword, getAuth,
+    GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword,
+    signInWithPopup, signOut, updateProfile, updatePassword, sendEmailVerification
+} from 'firebase/auth';
+import app from '../../Firebase/firebase.config';
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const createUser = (email, pass) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, pass)
+    }
+
+    const upDateUser = (userInfo) => {
+        setLoading(true)
+        return updateProfile(auth.currentUser, userInfo)
+    }
+
+    const userVerification = () => {
+        setLoading(true)
+        return sendEmailVerification(auth.currentUser)
+    }
+
+    const updatePass = (newPass) => {
+        setLoading(true)
+        return updatePassword(auth.currentUser, newPass)
+    }
+
+    const login = (email, pass) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, pass)
+    }
+
+    const signInWithGoogle = () => {
+        setLoading(true)
+        return signInWithPopup(auth, provider);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log(currentUser)
+            setUser(currentUser)
+            setLoading(false)
+        });
+        return () => unSubscribe();
+    }, [])
+
+    const LogOut = () => {
+        return signOut(auth);
+    }
+
+    const authInfo = {
+        createUser,
+        login,
+        user,
+        LogOut,
+        upDateUser,
+        loading,
+        signInWithGoogle,
+        updatePass,
+        setLoading,
+        userVerification
+    }
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
