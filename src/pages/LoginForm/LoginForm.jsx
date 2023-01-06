c983eba599760c48e28b9151d4f4aa9bc75ca0c6
mdsadikul <mdsadikulislam891@gmail.com>
2023-01-06 21:20:14 +0600

    Update-infoForm

--- src/pages/LoginForm/LoginForm.jsx
+++ src/pages/LoginForm/LoginForm.jsx
@@ -22,23 +22,23 @@ const LoginForm = () => {
     };
     return (
         <section className='container mx-auto my-12'>
-            <div className='py-12 justify-center flex items-center'>
-                <div className='bg-slate-200 p-5 border rounded-xl w-9/12 md:w-1/2 lg:w-4/12 mx-auto'>
-                    <form onSubmit={handleSubmit(handleLogin)} className='grid grid-cols-1 py-8 gap-1'>
-                        <h1 className='text-4xl text-center font-bold'>Login</h1>
+            <div className='justify-center flex items-center'>
+                <div className='bg-base-200 p-5 border rounded-xl w-9/12 md:w-1/2 lg:w-5/12 mx-auto'>
+                    <form onSubmit={handleSubmit(handleLogin)} className='grid grid-cols-1 py-6 gap-1'>
+                        <h1 className='text-4xl text-center font-bold'>Login Now</h1>
                         <div className="form-control">
                             <p className='text-red-600'>{loginError}</p>
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" {...register("email", { required: "Email Address is required" })} placeholder="email address" className="input input-bordered" />
+                            <input type="email" {...register("email", { required: "Email Address is required" })} placeholder="email address" className="bg-gray-100 input input-bordered" />
                             {errors.email && <p className='text-red-600' role="alert">{errors.email?.message}</p>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" {...register("password", { required: "Password is required" })} placeholder="password" className="input input-bordered" />
+                            <input type="password" {...register("password", { required: "Password is required" })} placeholder="password" className="bg-gray-100 input input-bordered" />
                             {errors.password && <p role="alert" className='text-red-600'>{errors.password?.message}</p>}
                         </div>
                         <input className='btn btn-outline text-black font-bold mt-4' type="submit" value="Login" />
--- src/pages/SingUpForm/SignUpForm.jsx
+++ src/pages/SingUpForm/SignUpForm.jsx
@@ -53,8 +53,8 @@ const SingUpForm = () => {
     };
     return (
         <section className='container mx-auto my-12'>
-            <div className='py-12 justify-center flex items-center'>
-                <div className='bg-slate-200 p-5 border rounded-xl w-9/12 md:w-1/2 lg:w-4/12 mx-auto'>
+            <div className='justify-center flex items-center'>
+                <div className='bg-base-200 p-5 border rounded-xl w-9/12 md:w-1/2 lg:w-5/12 mx-auto'>
                     <form onSubmit={handleSubmit(handleSignUP)} className='grid grid-cols-1 py-8 gap-1'>
                         <h1 className='text-4xl text-center font-bold'>Register Now</h1>
                         <div className="form-control">
@@ -62,7 +62,7 @@ const SingUpForm = () => {
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" {...register("name", { required: "Name is required"})} placeholder="your name" className="bg-slate-100 input input-bordered" />
+                            <input type="text" {...register("name", { required: "Name is required"})} placeholder="your name" className="bg-gray-100 input input-bordered" />
                             {errors.name && <p className='text-red-600' role="alert">{errors.name?.message}</p>} 
                             <label className="label">
                                 <span className="label-text">Picture</span>
@@ -72,17 +72,17 @@ const SingUpForm = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" {...register("email", { required: "Email Address is required"})} placeholder="email address" className="bg-slate-100 input input-bordered" />
+                            <input type="email" {...register("email", { required: "Email Address is required"})} placeholder="email address" className="bg-gray-100 input input-bordered" />
                             {errors.email && <p className='text-red-600' role="alert">{errors.email?.message}</p>} 
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" {...register("password", { required: "Password is required"})} placeholder="password" className="bg-slate-100 input input-bordered" />
+                            <input type="password" {...register("password", { required: "Password is required"})} placeholder="password" className="bg-gray-100 input input-bordered" />
                             {errors.password && <p role="alert" className='text-red-600'>{errors.password?.message}</p>}
                         </div>
-                        <input className='btn btn-outline text-black font-bold mt-4' type="submit" value="Sign Up" />
+                        <input className='btn btn-outline text-black font-bold mt-4' type="submit" value="Next" />
                     </form>
                     <p className='font-bold mb-4 text-lg text-center'>Already have an account? <Link to='/login' className='link link-hover text-red-600'>Login Now</Link></p>
                 </div>
