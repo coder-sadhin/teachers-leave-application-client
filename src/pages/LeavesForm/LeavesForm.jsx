4d374f7226934728236bfb5d2a096745366c47eb
mdsadikul <mdsadikulislam891@gmail.com>
2023-01-07 00:00:29 +0600

    update leavesForm

--- src/pages/LeavesForm/LeavesForm.jsx
+++ src/pages/LeavesForm/LeavesForm.jsx
@@ -3,34 +3,28 @@ import { useForm } from 'react-hook-form';
 
 const LeavesForm = () => {
     const {register, handleSubmit, formState: {errors}} = useForm();
-    const [sex, setSex] = useState('');
-    const [religion, setReligion] = useState('');
 
     const handleSave = data => {
 
-        const name = data.name;
-        const idNo = data.idNo;
         const department = data.department;
         const shift = data.shift;
+        const leaves = data.leaves;
         const title = data.title;
-        const phone = data.phone;
-        const email = data.email;
-        const birthday = data.date;
-        const address = data.address;
+        const startDate = data.startDate;
+        const endDate = data.endDate;
+        const totalDays = data.totalDays;
+        const description = data.description;
 
 
         const details = {
-            name,
-            idNo,
             department,
             shift,
+            leaves,
             title,
-            phone,
-            email,
-            birthday,
-            address,
-            sex,
-            religion
+            startDate,
+            endDate,
+            totalDays,
+            description
         }
     
         console.log(details)
@@ -40,8 +34,8 @@ const LeavesForm = () => {
             <div className=" bg-base-200 py-16">
                 <div className="hero-content flex-col">
                     <form onSubmit={handleSubmit(handleSave)} className="card w-full">
-                        <h3 className='w-11/12 mx-auto text-xl md:text-2xl lg:text-3xl font-semibold text-start md:mb-2'>You have <span className='font-bold'>3 leaves</span> left this months</h3>
-                    <div className="divider w-11/12 mx-auto"></div>
+                        <h3 className='w-11/12 mx-auto text-xl md:text-2xl lg:text-3xl font-semibold text-start mb-6'>You have <span className='font-bold'>3 leaves</span> left this month</h3>
+                        <div className="divider w-11/12 mx-auto"></div>
                         <div className="card-body grid grid-cols-1 md:grid-cols-2 gap-8">
                             <div className="form-control">
                                 <label className="label">
@@ -97,24 +91,31 @@ const LeavesForm = () => {
                                 <label className="label">
                                     <span className="label-text">Start date<span className='text-red-600'>*</span></span>
                                 </label>
-                                <input type="date" {...register("date", { required: "Start date is required"})} placeholder="Your Birthday" className="bg-gray-100 input input-bordered" />
-                                {errors.date && <p role="alert" className='text-red-600'>{errors.date?.message}</p>}
+                                <input type="date" {...register("startDate", { required: "Start date is required"})} placeholder="Your Birthday" className="bg-gray-100 input input-bordered" />
+                                {errors.startDate && <p role="alert" className='text-red-600'>{errors.startDate?.message}</p>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">End date<span className='text-red-600'>*</span></span>
                                 </label>
-                                <input type="date" {...register("date", { required: "End date is required"})} placeholder="Your Birthday" className="bg-gray-100 input input-bordered" />
-                                {errors.date && <p role="alert" className='text-red-600'>{errors.date?.message}</p>}
+                                <input type="date" {...register("endDate", { required: "End date is required"})} placeholder="Your Birthday" className="bg-gray-100 input input-bordered" />
+                                {errors.endDate && <p role="alert" className='text-red-600'>{errors.endDate?.message}</p>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">No. of days leaves required<span className='text-red-600'>*</span></span>
                                 </label>
-                                <input type="number" {...register("totalDay", { required: "Total days is required"})} placeholder="Enter no. of leaves" className="bg-gray-100 input input-bordered" />
-                                {errors.totalDay && <p role="alert" className='text-red-600'>{errors.totalDay?.message}</p>}
+                                <input type="number" {...register("totalDays", { required: "Total days is required"})} placeholder="Enter no. of leaves" className="bg-gray-100 input input-bordered" />
+                                {errors.totalDays && <p role="alert" className='text-red-600'>{errors.totalDays?.message}</p>}
                             </div>
-                            <div className="form-control mt-9">
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Reason for Leave<span className='text-red-600'>*</span></span>
+                                </label>
+                                <textarea {...register("description", { required: "Reason for leave is required"})} className="textarea bg-gray-100 input-bordered" placeholder="Description"></textarea>
+                                {errors.description && <p role="alert" className='text-red-600'>{errors.description?.message}</p>}
+                            </div>
+                            <div className="form-control mt-4">
                                 <button type='submit' className="btn btn-primary">Submit</button>
                             </div>
                         </div>
