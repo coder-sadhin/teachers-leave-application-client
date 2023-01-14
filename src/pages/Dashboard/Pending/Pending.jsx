0c25ccd33dbf73ea577e585474405b69aeaa1667
coder-sadhin <codersadhin@gmail.com>
2023-01-15 00:39:51 +0600

    leaves data by dainamic

--- src/pages/Dashboard/Pending/Pending.jsx
+++ src/pages/Dashboard/Pending/Pending.jsx
@@ -6,9 +6,9 @@ import { AuthContext } from '../../../ContextApi/AuthProvider/AuthProvider';
 import { serverApi } from '../../../ServerApi/ServerApi';
 
 const Pending = () => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
-    const {data: leaves = [], isLoading, refetch} = useQuery({
+    const { data: leaves = [], isLoading, refetch } = useQuery({
         queryKey: ['pendingLeave'],
         queryFn: async () => {
             const res = await fetch(`${serverApi}/pendingLeave?email=${user?.email}`);
@@ -16,37 +16,46 @@ const Pending = () => {
             return data;
         }
     });
-    console.log(user);
     console.log(leaves);
-
-    if(isLoading) {
+    if (isLoading) {
         return <Spinner />
     }
-    
+
     return (
-        <section className='bg-slate-300 py-8 md:rounded-lg'>
-            <div className='w-11/12 mx-auto mt-8'>
-                <h1 className='text-2xl font-bold text-center mb-8'>Requeted to You</h1>
-                <div>
-                    <div className='flex items-center'>
-                        <img className='w-12 h-12 rounded-full bg-cyan-500 p-2 mr-4' src={calenderLogo} alt="" />
-                        <h2 className='text-sm md:text-xl font-bold'>Saturday, May 23 2023
-                            <span className='text-gray-500'> to </span>
-                            Monday, May 25 2023
-                        </h2>
+        <div className='w-11/12 mx-auto'>
+            <section className='bg-slate-300 flex justify-center items-center mb-8 md:rounded-md'>
+                <h1 className='text-2xl font-bold text-center py-3'>Requeted to You</h1>
+            </section>
+
+            {
+                leaves.map(leave =>
+                    <div className='' key={leave._id}>
+                        <section className='bg-slate-300 my-5 py-5 md:rounded-lg'>
+                            <div className='w-11/12 mx-auto my-8'>
+                                <div className='flex  items-center'>
+                                    <img className='w-12 h-12 rounded-full bg-cyan-500 p-2 mr-4' src={calenderLogo} alt="" />
+                                    <h2 className='text-sm md:text-xl font-bold'>{leave?.startDate}
+                                        <span className='text-gray-500'> to </span>
+                                        {leave?.endDate}
+                                    </h2>
+                                </div>
+                                <div className='ml-20'>
+                                    <p className='text-[14px] md:text-[16px] text-justify mb-8'>{leave.description}</p>
+                                    <div className='flex justify-between items-center'>
+                                        <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-b-3xl font-bold hidden md:block'>Aprove</button>
+                                        <button className='btn btn-outline border-1 border-red-600 text-black hover:bg-red-600 rounded-b-3xl font-bold hidden md:block'>Cancle</button>
+                                        <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-b-2xl font-bold btn-sm md:hidden'>Aprove</button>
+                                        <button className='btn btn-outline border-1 border-red-600 text-black hover:bg-red-600 rounded-b-2xl font-bold btn-sm md:hidden'>Cancle</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
                     </div>
-                    <div className='ml-20'>
-                        <p className='text-[14px] md:text-[16px] text-justify mb-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, explicabo quibusdam voluptatem nihil fugit at eveniet perspiciatis, cum omnis voluptatibus, eos ipsum veritatis dignissimos? Nam possimus incidunt quia rem reprehenderit!</p>
-                        <div className='flex justify-between items-center'>
-                            <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-b-3xl font-bold hidden md:block'>Aprove</button>
-                            <button className='btn btn-outline border-1 border-red-600 text-black hover:bg-red-600 rounded-b-3xl font-bold hidden md:block'>Cancle</button>
-                            <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-b-2xl font-bold btn-sm md:hidden'>Aprove</button>
-                            <button className='btn btn-outline border-1 border-red-600 text-black hover:bg-red-600 rounded-b-2xl font-bold btn-sm md:hidden'>Cancle</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+                )
+            }
+
+
+        </div>
     );
 };
 
