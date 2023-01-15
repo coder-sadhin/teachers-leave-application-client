ff949dde52140c21a152652683fcc5913d4ab961
mdsadikul <mdsadikulislam891@gmail.com>
2023-01-15 22:05:45 +0600

    added pending table and history table

--- src/pages/Dashboard/History/History.jsx
+++ src/pages/Dashboard/History/History.jsx
@@ -15,12 +15,12 @@ const History = () => {
             return data;
         }
     });
-    // console.log(leaves);
+   
     if (isLoading) {
         return <Spinner />
     };
 
-    // console.log(leaves);
+    console.log(leaves);
     return (
         <div className='w-11/12 mx-auto'>
             <section className='bg-slate-300 flex justify-center items-center mb-8 md:rounded-md'>
@@ -28,35 +28,30 @@ const History = () => {
             </section>
 
             {
-                leaves.map(leave =>
-                    <div className='' key={leave._id}>
-                        <section className='bg-slate-300 my-5 py-5 md:rounded-lg'>
-                            <div className='w-11/12 mx-auto my-8'>
-                                <div className='flex  items-center'>
-                                    <img className='w-12 h-12 rounded-full bg-cyan-500 p-2 mr-4' src={calenderLogo} alt="" />
-                                    <h1 className='text-[16px] md:text-2xl text-justify font-semibold'>{leave?.name}</h1>
-                                </div>
-                                <h1 className='text-lg text-justify font-semibold ml-16 mt-[-10px] mb-2'>{leave?.title}</h1>
-                                <div className='ml-16'>
-                                    <h2 className='text-[16px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Date: </span>{leave?.startDate}
-                                        <span className='text-red-500'> to </span>
-                                        {leave?.endDate}
-                                    </h2>
-                                    <p className='text-[14px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Total day(s): </span><span className='text-primary'>{leave?.totalDays}</span></p>
-                                    <p className='text-[14px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Leave category: </span>{leave?.leaves_C}</p>
-                                    <p className='text-[14px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Department: </span>{leave?.department}</p>
-                                    <p className='text-[14px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Shift: </span>{leave?.shift}</p>
-                                    <p className='text-[16px] md:text-[16px] text-justify my-8'>{leave?.description}</p>
-                                    
-                                    {/* <div className='flex justify-between items-center'>
-                                        <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-b-3xl font-bold hidden md:block'>Aprove</button>
-                                        
-                                    </div> */}
-                                </div>
-                            </div>
-                        </section>
-                    </div>
-                )
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+                        <thead>
+                        <tr>
+                            <th>Profile</th>
+                            <th className='text-center'>Name</th>
+                            <th className='text-center'>Title</th>
+                            <th className='text-center'>Date</th>
+                            <th className='text-center'>Total Day(s)</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                leaves.map((leave, index) => <tr key={leave?._id}>
+                                    <td><img className='w-12 h-12 rounded-full' src={leave?.image} alt="" /></td>
+                                    <td className='text-center'>{leave?.name}</td>
+                                    <td className='text-center'>{leave?.title}</td>
+                                    <td className='text-center'>{leave?.startDate} <span className='text-red-600 font-semibold'>to</span> {leave?.endDate}</td>
+                                    <td className='text-center'>{leave?.totalDays}</td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             }
         </div>
     );
--- src/pages/Dashboard/Pending/Pending.jsx
+++ src/pages/Dashboard/Pending/Pending.jsx
@@ -17,11 +17,16 @@ const Pending = () => {
         }
     });
     // console.log(leaves);
+
+
     if (isLoading) {
         return <Spinner />
     };
 
-    // console.log(leaves);
+    if(!isLoading){
+        refetch();
+    }
+    console.log(leaves);
 
     return (
         <div className='w-11/12 mx-auto'>
@@ -33,28 +38,42 @@ const Pending = () => {
                 leaves.map(leave =>
                     <div className='' key={leave._id}>
                         <section className='bg-slate-300 my-5 py-5 md:rounded-lg'>
-                            <div className='w-11/12 mx-auto my-8'>
+                            <div className='w-11/12 mx-auto my-4'>
                                 <div className='flex  items-center'>
-                                    <img className='w-12 h-12 rounded-full bg-cyan-500 p-2 mr-4' src={calenderLogo} alt="" />
+                                    <img className='w-12 h-12 rounded-full bg-cyan-500 mr-4' src={leave?.image} alt="" />
                                     <h1 className='text-[16px] md:text-2xl text-justify font-semibold'>{leave?.name}</h1>
                                 </div>
-                                <h1 className='text-lg text-justify font-semibold ml-16 mt-[-10px] mb-2'>{leave?.title}</h1>
-                                <div className='ml-16'>
-                                    <h2 className='text-[16px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Date: </span>{leave?.startDate}
-                                        <span className='text-red-500'> to </span>
-                                        {leave?.endDate}
-                                    </h2>
-                                    <p className='text-[14px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Total day(s): </span><span className='text-primary'>{leave?.totalDays}</span></p>
-                                    <p className='text-[14px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Leave category: </span>{leave?.leaves_C}</p>
-                                    <p className='text-[14px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Department: </span>{leave?.department}</p>
-                                    <p className='text-[14px] md:text-[16px] text-justify font-bold'><span className='text-gray-600 font-semibold'>Shift: </span>{leave?.shift}</p>
-                                    <p className='text-[16px] md:text-[16px] text-justify my-8'>{leave?.description}</p>
-                                    
-                                    <div className='flex justify-between items-center'>
-                                        <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-b-3xl font-bold hidden md:block'>Aprove</button>
-                                        <button className='btn btn-outline border-1 border-red-600 text-black hover:bg-red-600 rounded-b-3xl font-bold hidden md:block'>Cancle</button>
-                                        <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-b-2xl font-bold btn-sm md:hidden'>Aprove</button>
-                                        <button className='btn btn-outline border-1 border-red-600 text-black hover:bg-red-600 rounded-b-2xl font-bold btn-sm md:hidden'>Cancle</button>
+                                <h1 className='text-lg text-justify font-semibold ml-16 mt-[-10px] mb-4'>{leave?.title}</h1>
+                                <div className=''>
+                                    <div className="overflow-x-auto">
+                                        <table className="table w-full">
+                                            <thead>
+                                            <tr>
+                                                <th className='text-center'> Start to End Date</th>
+                                                <th className='text-center'>Total Day(s)</th>
+                                                <th className='text-center'>Leave Category</th>
+                                                <th className='text-center'>Department</th>
+                                                <th className='text-center'>Shift</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                                {
+                                                    leaves.map((leave, index) => <tr key={leave?._id}>
+                                                        <td className='text-center'>{leave?.startDate} <span className='text-red-600 font-semibold'>to</span> {leave?.endDate}</td>
+                                                        <td className='text-center'>{leave?.totalDays}</td>
+                                                        <td className='text-center'>{leave?.leaves_C}</td>
+                                                        <td className='text-center'>{leave?.department}</td>
+                                                        <td className='text-center'>{leave?.shift}</td>
+                                                    </tr>)
+                                                }
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div className='flex justify-between items-center mt-8'>
+                                        <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-xl font-bold hidden md:block'>Aprove</button>
+                                        <button className='btn btn-outline border-1 border-red-600 text-black hover:bg-red-600 rounded-xl font-bold hidden md:block'>Cancle</button>
+                                        <button className='btn btn-outline border-1 border-green-600 text-black hover:bg-green-600 rounded-xl font-bold btn-sm md:hidden'>Aprove</button>
+                                        <button className='btn btn-outline border-1 border-red-600 text-black hover:bg-red-600 rounded-xl font-bold btn-sm md:hidden'>Cancle</button>
                                     </div>
                                 </div>
                             </div>
